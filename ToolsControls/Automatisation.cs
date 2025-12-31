171381006bbcfac812cae89cd6c93bf8508cf315
Hebreux11 <167058478+Hebreux11@users.noreply.github.com>
2025-12-31 13:59:37 +0200

    Add files via upload

---
+++ ToolsControls/Automatisation.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Configuration;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.Extensions.Configuration;
+
+
+namespace Hebreux11AppServer.ToolsControls
+{
+    public class Automatisation
+    {
+        private static string Date = DateTime.UtcNow.ToString("yyyy-MM-dd");
+        public static async Task XmlDocAsync()
+        {
+
+            XNamespace ns = "http://www.sitemaps.org/schemas/sitemap/0.9";
+
+            var urlset = new XElement(ns + "urlset",
+               new XElement(ns + "url",
+                   new XElement(ns + "loc", "https://www.hebreux11.com/"),
+                   new XElement(ns + "lastmod", Date),
+                   new XElement(ns + "changefreq", "weekly"),
+                   new XElement(ns + "priority", "1.0")
+               ),
+               new XElement(ns + "url",
+                   new XElement(ns + "loc", "https://www.hebreux11.com/Services/Apps"),
+                   new XElement(ns + "lastmod", Date),
+                   new XElement(ns + "changefreq", "weekly"),
+                   new XElement(ns + "priority", "0.8")
+               ),
+               new XElement(ns + "url",
+                   new XElement(ns + "loc", "https://www.hebreux11.com/Services/Photos"),
+                   new XElement(ns + "lastmod", Date),
+                   new XElement(ns + "changefreq", "weekly"),
+                   new XElement(ns + "priority", "0.8")
+               ),
+               new XElement(ns + "url",
+                   new XElement(ns + "loc", "https://www.hebreux11.com/Services/Videos"),
+                   new XElement(ns + "lastmod", Date),
+                   new XElement(ns + "changefreq", "weekly"),
+                   new XElement(ns + "priority", "0.8")
+               ),
+               new XElement(ns + "url",
+                   new XElement(ns + "loc", "https://www.hebreux11.com/Services/Formations"),
+                   new XElement(ns + "lastmod", Date),
+                   new XElement(ns + "changefreq", "weekly"),
+                   new XElement(ns + "priority", "0.8")
+               ),
+               new XElement(ns + "url",
+                   new XElement(ns + "loc", "https://www.hebreux11.com/Services/IT"),
+                   new XElement(ns + "lastmod", Date),
+                   new XElement(ns + "changefreq", "weekly"),
+                   new XElement(ns + "priority", "0.8")
+               ),
+               new XElement(ns + "url",
+                   new XElement(ns + "loc", "https://www.hebreux11.com/Evenements/Events"),
+                   new XElement(ns + "lastmod", Date),
+                   new XElement(ns + "changefreq", "weekly"),
+                   new XElement(ns + "priority", "0.8")
+               ),
+               new XElement(ns + "url",
+                   new XElement(ns + "loc", "https://www.hebreux11.com/Services/Quotation"),
+                   new XElement(ns + "lastmod", Date),
+                   new XElement(ns + "changefreq", "weekly"),
+                   new XElement(ns + "priority", "0.8")
+               ),
+               new XElement(ns + "url",
+                   new XElement(ns + "loc", "https://www.hebreux11.com/Services/Tarif"),
+                   new XElement(ns + "lastmod", Date),
+                   new XElement(ns + "changefreq", "weekly"),
+                   new XElement(ns + "priority", "0.8")
+               )
+           );
+
+            var doc = new XDocument(
+                new XDeclaration("1.0", "UTF-8", null),
+                urlset
+            );
+
+            // Écrit le fichier sitemap.xml dans le dossier courant
+            doc.Save("wwwroot/sitemap.xml");
+
+        }
+        public static void XmlDoc()
+        {
+
+            XNamespace ns = "http://www.sitemaps.org/schemas/sitemap/0.9";
+
+            var urlset = new XElement(ns + "urlset",
+                new XElement(ns + "url",
+                    new XElement(ns + "loc", "https://www.hebreux11.com/"),
+                    new XElement(ns + "lastmod", Date),
+                    new XElement(ns + "changefreq", "weekly"),
+                    new XElement(ns + "priority", "1.0")
+                ),
+                new XElement(ns + "url",
+                    new XElement(ns + "loc", "https://www.hebreux11.com/Services/Apps"),
+                    new XElement(ns + "lastmod", Date),
+                    new XElement(ns + "changefreq", "weekly"),
+                    new XElement(ns + "priority", "0.8")
+                ),
+                new XElement(ns + "url",
+                    new XElement(ns + "loc", "https://www.hebreux11.com/Services/Photos"),
+                    new XElement(ns + "lastmod", Date),
+                    new XElement(ns + "changefreq", "weekly"),
+                    new XElement(ns + "priority", "0.8")
+                ),
+                new XElement(ns + "url",
+                    new XElement(ns + "loc", "https://www.hebreux11.com/Services/Videos"),
+                    new XElement(ns + "lastmod", Date),
+                    new XElement(ns + "changefreq", "weekly"),
+                    new XElement(ns + "priority", "0.8")
+                ),
+                new XElement(ns + "url",
+                    new XElement(ns + "loc", "https://www.hebreux11.com/Services/Formations"),
+                    new XElement(ns + "lastmod", Date),
+                    new XElement(ns + "changefreq", "weekly"),
+                    new XElement(ns + "priority", "0.8")
+                ),
+                new XElement(ns + "url",
+                    new XElement(ns + "loc", "https://www.hebreux11.com/Services/IT"),
+                    new XElement(ns + "lastmod", Date),
+                    new XElement(ns + "changefreq", "weekly"),
+                    new XElement(ns + "priority", "0.8")
+                ),
+                new XElement(ns + "url",
+                    new XElement(ns + "loc", "https://www.hebreux11.com/Evenements/Events"),
+                    new XElement(ns + "lastmod", Date),
+                    new XElement(ns + "changefreq", "weekly"),
+                    new XElement(ns + "priority", "0.8")
+                ),
+                new XElement(ns + "url",
+                    new XElement(ns + "loc", "https://www.hebreux11.com/Services/Quotation"),
+                    new XElement(ns + "lastmod", Date),
+                    new XElement(ns + "changefreq", "weekly"),
+                    new XElement(ns + "priority", "0.8")
+                ),
+                new XElement(ns + "url",
+                    new XElement(ns + "loc", "https://www.hebreux11.com/Services/Tarif"),
+                    new XElement(ns + "lastmod", Date),
+                    new XElement(ns + "changefreq", "weekly"),
+                    new XElement(ns + "priority", "0.8")
+                )
+            );
+
+            var doc = new XDocument(
+                new XDeclaration("1.0", "UTF-8", null),
+                urlset
+            );
+
+            using var sw = new StringWriter();
+            using var xw = XmlWriter.Create(sw, new XmlWriterSettings { Encoding = Encoding.UTF8 });
+            doc.WriteTo(xw);
+            xw.Flush();
+            // return Results.Content(sw.ToString(), "application/xml");
+
+
+            // Écrit le fichier sitemap.xml dans le dossier courant
+            doc.Save("wwwroot/sitemap.xml");
+        }
+
+    }
+
+}
---
+++ ToolsControls/ValidationData.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Configuration;
+using Hebreux11AppServer.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace Hebreux11AppServer.ToolsControls
+{
+    public class ValidationData
+    {
+        public string MessageErreur { get; set; }
+        public bool ValidationFormulaire(FormulaireModel data)
+        {
+            if (string.IsNullOrWhiteSpace(data.NomClient))
+            {
+                MessageErreur = "Indiquer votre nom";
+                return false;
+            }
+            else if (string.IsNullOrWhiteSpace(data.Sujet))
+            {
+                MessageErreur = "Indiquer le sujet de votre requete";
+                return false;
+            }
+            else if (string.IsNullOrWhiteSpace(data.EmailClient))
+            {
+                MessageErreur = "Indiquer votre email";
+                return false;
+            }
+            else if (!data.EmailClient.Contains('@') && !data.EmailClient.Contains('.'))
+            {
+                MessageErreur = "Indiquer votre email valide";
+                return false;
+            }
+            else if (string.IsNullOrWhiteSpace(data.Services))
+            {
+                MessageErreur = "Choisissez un service";
+                return false;
+            }
+            else if (string.IsNullOrWhiteSpace(data.MessageClient))
+            {
+                MessageErreur = "Indiquer votre message en quelque mots";
+                return false;
+            }
+            else
+                return true;
+
+        }
+
+        internal bool ValidationEvent(InvitesModel data)
+        {
+            if (string.IsNullOrWhiteSpace(data.NomInviter))
+            {
+                return false;
+            }
+            else if (string.IsNullOrWhiteSpace(data.TypeInviter) || data.TypeInviter == "-------Selectionner ici-------")
+            {
+                return false;
+            }
+            else if (string.IsNullOrWhiteSpace(data.TableInviter.NomTable) || data.TableInviter.NomTable == "-------Selectionner ici-------")
+            {
+                return false;
+            }
+            else
+                return true;
+        }
+    }
+
+}
+
+
